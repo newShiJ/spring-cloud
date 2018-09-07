05f42aa24d37ed974f23c7fe6c7fee9b2dd88d05
chenmingming <chenmingming@hyperchain.cn>
2018-09-07 10:54:03 +0800

    更新项目目录结构

---
+++ config/config-client/src/main/java/learnspring/configclient/ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package learnspring.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ config/config-client/src/main/java/learnspring/configclient/controller/EchoController.java
@@ -0,0 +1,14 @@
+package learnspring.configclient.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * 输出配置项内容
+ * @author chenmingming
+ * @date 2018/9/7
+ */
+@RestController
+public class EchoController {
+
+}
