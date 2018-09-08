9ac3949a790a6955b07be23f007dde8eebb07a78
chenmingming <chenmingming@hyperchain.cn>
2018-09-08 12:44:01 +0800

    健康检查

--- config/config-client/src/main/java/learnspring/configclient/ConfigClientApplication.java
+++ config/config-client/src/main/java/learnspring/configclient/ConfigClientApplication.java
@@ -1,12 +1,48 @@
 package learnspring.configclient;
 
+import learnspring.configclient.health.MyHealthIndicator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
 @SpringBootApplication
+@Component
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    private final ContextRefresher contextRefresher;
+
+    private final Environment environment;
+    @Autowired
+    public ConfigClientApplication(ContextRefresher contextRefresher, Environment environment) {
+        this.contextRefresher = contextRefresher;
+        this.environment = environment;
+    }
+
+    //@Scheduled(fixedRate = 5 * 1000, initialDelay = 3 * 1000)
+    public void autoRefresh() {
+        Set<String> refresh = contextRefresher.refresh();
+        for (String name : refresh) {
+            String format = String.format("Thread:%s 配置刷新 key:%s => value:%s",
+                    Thread.currentThread().getName(),
+                    name,
+                    environment.getProperty(name));
+            System.err.println(format);
+        }
+    }
+
+    @Bean
+    public MyHealthIndicator myHealth(){
+        return new MyHealthIndicator();
     }
 }
--- config/config-client/src/main/java/learnspring/configclient/controller/EchoController.java
+++ config/config-client/src/main/java/learnspring/configclient/controller/EchoController.java
@@ -1,5 +1,8 @@
 package learnspring.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2018/9/7
  */
 @RestController
+@RefreshScope
 public class EchoController {
+    @Value("${my.name}")
+    private String myName;
 
+    @GetMapping("/myName")
+    public String getMyName(){
+        return myName;
+    }
 }
